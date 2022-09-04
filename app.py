d4607a062b4ce5670ef105e952e521f49bc2d9de
Vignesh-Gopinathan <vickygvignesh96@gmail.com>
2022-09-04 19:10:43 +0200

    bash script

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
 import streamlit as st
 from transformers import pipeline, AutoTokenizer, TFAutoModelForSeq2SeqLM
-
-'''
-def download_model():
-    with open("app_config.yaml", 'r') as f:
-        dirs = yaml.load(f, Loader=yaml.FullLoader)
-        cloud_model_dir = dirs['cloud_model_dir']
-
-    storage_client = storage.Client()
-    buckets = list(storage_client.list_buckets())
-
-    print('\nBuckets:')
-    for bucket in buckets:
-        print('\t', bucket.name)
-
-        blobs = storage_client.list_blobs(bucket)
-        for blob in blobs:
-            if cloud_model_dir in blob.name:
-                if blob.name.endswith('.h5') or blob.name.endswith('.json'):
-                    destination_file_name = 'models/' + blob.name.split('/')[-1]
-                    blob.download_to_filename(destination_file_name)
-
-    print('All models downloaded from Cloud storage')
-'''
+from pathlib import Path
 
 
 @st.cache(allow_output_mutation=True)
@@ -34,29 +12,18 @@ def load_model():
     return translator_pipe
 
 
-'''
-if list(Path('models/').glob('*.h5')) and list(Path('models/').glob('*.json')):
-    st.write('Model already download. Loading model.....')
-    translator = load_model()
-else:
-    st.write('Model not found. Downloading model.....')
-    download_model()
-    if not(list(Path('models/').glob('*.h5')) and list(Path('models/').glob('*.json'))):
-        st.write('Downloaded model not found')
-        st.write(list(Path('models/').glob('*')))
-    st.write('Downloaded model. Loading model.....')
-    translator = load_model()
-'''
-
 if __name__ == '__main__':
     st.title('Text translator from English to French')
-    translator = load_model()
-    text = st.text_input('Input text (required):', 'Please enter the text to be translated here.')
-    if not text:
-        st.warning("Please fill out so required fields")
+    if list(Path('models/').glob('*.h5')) and list(Path('models/').glob('*.json')):
+        translator = load_model()
+        text = st.text_input('Input text (required):', 'Please enter the text to be translated here.')
+        if not text:
+            st.warning("Please fill out so required fields")
 
-    with st.spinner('Translating...'):
-        if st.button('Translate'):
-            result = translator(text)
-            st.write('Translated text:')
-            st.write(result[0]['translation_text'])
+        with st.spinner('Translating...'):
+            if st.button('Translate'):
+                result = translator(text)
+                st.write('Translated text:')
+                st.write(result[0]['translation_text'])
+    else:
+        st.write('Model not found')
